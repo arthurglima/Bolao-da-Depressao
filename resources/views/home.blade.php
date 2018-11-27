02107e9ff17e39fc8357c8554b0156c9351019f3
Israel <iisraell@live.com>
2018-11-27 19:38:53 -0300

    Estrutura do sistema

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Dashboard</div>
+                <div class="card-header">Mural</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -13,7 +13,7 @@
                         </div>
                     @endif
 
-                    You are logged in!
+                    Loguei
                 </div>
             </div>
         </div>
